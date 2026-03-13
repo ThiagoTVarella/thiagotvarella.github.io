0adafbd99f382ea69769e9d08bc22f94586a72f6
ThiagoTVarella <thiago_tv2@hotmail.com>
2026-03-13 11:14:47 -0700

    Deploy: remove question-specific examples from system prompt

--- Caramelo/demo/flutter_bootstrap.js
+++ Caramelo/demo/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3543c00c801c49c78a0d4e7ceafbe278c9c26f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1725280367" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "178270487" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
